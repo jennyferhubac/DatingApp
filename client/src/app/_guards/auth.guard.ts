96b30e6ee2bd7f8b8753bdbb041143f4641f7bdd
jennyferhubac <jennyfer.hubac@gmail.com>
2023-05-26 10:18:54 -0400

    End of Section 6

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private accountService: AccountService, private toastr: ToastrService){
+    
+  }
+  /*
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  */
+ canActivate(): Observable<boolean> {
+  //auth guard automatically subrscibe to an observable
+    return this.accountService.currentUser$.pipe(
+      map((user) => {
+        if(user) return true;
+        else {
+          this.toastr.error('You shall not pass!');
+          return false;
+        }
+      })
+    )
+  }
+
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { PowerBIEmbedModule } from 'powerbi-client-angular';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -18,7 +22,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +35,8 @@ import { RegisterComponent } from './register/register.component';
     FormsModule,
     PowerBIEmbedModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
